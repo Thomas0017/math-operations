b4d55a89cb8b99c1a8c6626f0fcfe64bee44a3b7
Thomas0017 <43010203+Thomas0017@users.noreply.github.com>
2018-09-25 12:50:15 -0400

    Add files via upload

---
+++ OperationsPractice.py
@@ -0,0 +1,21 @@
+'''
+Created on Sep 25, 2018
+
+@author: Thomas Pietra
+Operations parctice
+'''
+
+print("Welcome to the test score calculator.")
+print("\n\n\n\n")
+test1=float(input("Please enter the first test score: "))
+test2=float(input("Please enter the second test score: "))
+test3=float(input("Please enter the third test score: "))
+
+#calculate the average
+average=(test1+test2+test3)/3
+
+#display the output
+print("Average test score:",average)
+
+input("Press enter to calculate a slope)
+
