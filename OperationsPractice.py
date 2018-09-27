0798c0e1caa7a6392584c7d4228ebdad0a276e7c
Thomas0017 <43010203+Thomas0017@users.noreply.github.com>
2018-09-27 12:41:49 -0400

    Add files via upload

--- OperationsPractice.py
+++ OperationsPractice.py
@@ -17,5 +17,21 @@ average=(test1+test2+test3)/3
 #display the output
 print("Average test score:",average)
 
-input("Press enter to calculate a slope)
 
+input("Press enter to find the slope between two points")
+
+x1=int(input("Please enter x1: "))
+x2=int(input("Please enter x2: "))
+y1=int(input("Please enter y1: "))
+y2=int(input("Please enter y2: "))
+
+dx=x2-x1
+dy=y2-y1
+
+print("The slope is",dy,"/",dx)
+
+
+price=float(input("please input the price: "))
+tax=price*0.06
+total=price+tax
+print("the tax is $",tax,"the total is $",total)
